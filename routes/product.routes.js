f61374d6c627f541a43f86d97fec7d86e506876a
sajid1545 <clashking1545@gmail.com>
2023-07-18 18:09:49 +0600

    authorization middleware created

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const productController = require("../controllers/product.controller");
 const upload = require("../middlewares/uploader");
+const verifyToken = require("../middlewares/verifyToken");
+const authorization = require("../middlewares/authorization");
 
 const router = express.Router();
 
 router.post("/file-upload", upload.array("image"), productController.fileUpload);
-router.route("/").get(productController.getAllProducts).post(productController.createProduct);
+router
+	.route("/")
+	.get(productController.getAllProducts)
+	.post(verifyToken, authorization("admin", "store-manager"), productController.createProduct);
 
 router.route("/bulk-update").patch(productController.bulkUpdateProducts);
 
